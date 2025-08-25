e19275dabfba07a7988fc2a16008ea674e6000a1
Mark Robinson <M-Robinson97@outlook.com>
2025-08-25 16:31:37 +0100

    feat: create interfaces for service and repository

--- src/main/java/com/test_apps/motorola_coding_challenge/repository/FileRepository.java
+++ src/main/java/com/test_apps/motorola_coding_challenge/repository/FileRepository.java
@@ -1,4 +1,16 @@
 package com.test_apps.motorola_coding_challenge.repository;
 
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface FileRepository {
+    List<String> getAllNames();
+
+    Resource get(String fileName);
+
+    String save(MultipartFile file);
+
+    void delete(String fileName);
 }
--- src/main/java/com/test_apps/motorola_coding_challenge/service/FileService.java
+++ src/main/java/com/test_apps/motorola_coding_challenge/service/FileService.java
@@ -1,4 +1,16 @@
 package com.test_apps.motorola_coding_challenge.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface FileService {
+    List<String> listFiles();
+
+    Resource getFile(String fileName);
+
+    String postFile(MultipartFile file);
+
+    void deleteFile(String fileName);
 }
