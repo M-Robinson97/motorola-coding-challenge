90993b9d770f67ffba11e61a2037971565f15cac
Mark Robinson <M-Robinson97@outlook.com>
2025-08-25 19:21:40 +0100

    feat: create basic save method

--- src/main/java/com/test_apps/motorola_coding_challenge/repository/FileRepositoryImpl.java
+++ src/main/java/com/test_apps/motorola_coding_challenge/repository/FileRepositoryImpl.java
@@ -1,15 +1,21 @@
 package com.test_apps.motorola_coding_challenge.repository;
 
+import com.test_apps.motorola_coding_challenge.service.StorageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.*;
 import java.util.List;
 
 @Slf4j
 @Repository
+@RequiredArgsConstructor
 public class FileRepositoryImpl implements FileRepository {
+    private final StorageService storageService;
+
     @Override
     public List<String> getAllNames() {
         log.info("Getting all files by name");
@@ -24,8 +30,15 @@ public class FileRepositoryImpl implements FileRepository {
 
     @Override
     public String save(MultipartFile file) {
-        log.info("Saving file with name: {}", file.getName());
-        return "";
+        final String fileName = file.getName();
+        log.info("Saving file with name: {}", fileName);
+        try {
+            Files.copy(file.getInputStream(), storageService.buildFilePath(fileName), StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            log.info("Saving failed for file {} with message: {}", fileName, e.getMessage());
+            return null;
+        }
     }
 
     @Override
