b0c692ce4b969971b9ec23b4d1c9eff3ee5561cb
Mark Robinson <M-Robinson97@outlook.com>
2025-08-31 19:51:20 +0100

    feat: add controller smoke check

--- src/test/java/com/test_apps/motorola_coding_challenge/MotorolaCodingChallengeApplicationTests.java
+++ src/test/java/com/test_apps/motorola_coding_challenge/MotorolaCodingChallengeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.test_apps.motorola_coding_challenge;
 
+import com.test_apps.motorola_coding_challenge.controller.FileManagerController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class MotorolaCodingChallengeApplicationTests {
+    @Autowired
+    private FileManagerController controller;
 
 	@Test
 	void contextLoads() {
+        assertThat(controller).isNotNull();
 	}
 
 }
